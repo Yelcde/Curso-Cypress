8e6c1b96324611daf7edf4233047258583e08f55
Yelcde <johnnercastrojy@gmail.com>
2025-04-09 10:44:30 -0300

    Terminando estudos sobre describe

--- cypress/integration/describe.spec.js
+++ cypress/integration/describe.spec.js
@@ -6,7 +6,9 @@ it("An external test...", () => {
 
 describe("Should group test...", () => {
     describe("Should group more especific test...", () => {
-        
+        it("A specific test...", () => {
+            
+        })
     })
 
     it("An internal test...", () => {
