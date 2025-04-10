143f2a913ddc089e25fd8e259d7970e2f18789dd
Yelcde <johnnercastrojy@gmail.com>
2025-04-10 17:49:37 -0300

    Estudando sobre retentativas (retrys) com Cypress

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -13,5 +13,11 @@ describe("Sync tests", () => {
         cy.get('#novoCampo').should('exist')
         cy.get('#novoCampo').type('funciona')
     })
+
+    it.only("Deve fazer retrys", () => {
+    // Este trecho de código não funciona porque o Cypress avalia as asserções de forma encadeada imediatamente. A asserção `should('not.exist')` verifica se o elemento com o seletor '#novoCampo' não existe no DOM, mas logo em seguida, a asserção `should('exist')` espera que o mesmo elemento exista no DOM. Como o Cypress não espera automaticamente entre essas asserções, o código falha se o elemento '#novoCampo' não aparecer instantaneamente após o clique no botão '#buttonDelay'.
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist').should("exist")
+    })
     
 })
