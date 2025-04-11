e8f74bb9fc8fc24f7dca8d6be702c28a79d014b7
Yelcde <johnnercastrojy@gmail.com>
2025-04-11 08:20:52 -0300

    Aprendendo a usar find com Cypress

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -14,10 +14,22 @@ describe("Sync tests", () => {
         cy.get('#novoCampo').type('funciona')
     })
 
-    it.only("Deve fazer retrys", () => {
+    it("Deve fazer retrys", () => {
     // Este trecho de código não funciona porque o Cypress avalia as asserções de forma encadeada imediatamente. A asserção `should('not.exist')` verifica se o elemento com o seletor '#novoCampo' não existe no DOM, mas logo em seguida, a asserção `should('exist')` espera que o mesmo elemento exista no DOM. Como o Cypress não espera automaticamente entre essas asserções, o código falha se o elemento '#novoCampo' não aparecer instantaneamente após o clique no botão '#buttonDelay'.
         cy.get('#buttonDelay').click()
         cy.get('#novoCampo').should('not.exist').should("exist")
+    })
+
+    it.only("Uso do Find", () => {
+        // Código que eu fiz sem assistir a resolução
+        // cy.get('#buttonList').click().should("exist", "span")
+        // cy.get('#lista').should("contain", "Item 1")
+        // cy.get('#lista li').should("exist", "span").should("contain", "Item 2")
+
+        // Código que o professor fez
+        cy.get('#buttonList').click()
+        cy.get('#lista li').find("span").should("contain", "Item 1")
+        cy.get('#lista li span').should("contain", "Item 2") // O segundo não pode ter o find, pois o span já é filho do li e como ele ja esta olhando para o li, não precisa do find
     })
     
 })
