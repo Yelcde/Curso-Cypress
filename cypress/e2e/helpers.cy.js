3570191b7d2652207025f50ebde234d5823b7386
yelcde <johnner.castro@assert.ifpb.edu.br>
2025-05-30 09:43:49 -0300

    Trabalhando com Its

--- cypress/e2e/helpers.cy.js
+++ cypress/e2e/helpers.cy.js
@@ -18,7 +18,14 @@ describe('Helpers...', () => {
     })
 
     it.only("Its...", () => {
-        const obj = {nome: "User", idade: 20}
-        cy.wrap(obj).should('have.property', 'nome', 'User')
+        // const obj = {nome: "User", idade: 20}
+        // cy.wrap(obj).should('have.property', 'nome', 'User')
+        // cy.wrap(obj).its('nome').should('be.equal', 'User')
+
+        // const obj = {nome: "User", idade: 20, endereco: {rua: 'Rua 1'}}
+        // cy.wrap(obj).its('endereco').its('rua').should('contains', 'R')
+
+        cy.visit("https://wcaquino.me/cypress/componentes.html")
+        cy.title().its('length').should('be.equal', 20)
     })
 })
